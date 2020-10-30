5f2deccd6af636d8a92fe2ac811e41b39f6ddf76
ytang18 <dannytang913@gmail.com>
2020-10-29 20:52:29 -0700

    update

---
+++ Lab_7/js/getSessionData.js
@@ -0,0 +1,10 @@
+// Get saved data from sessionStorage
+let name = ""; // Your code here
+let semester =  ""; // Your code here
+let credits =  ""; // Your code here
+let description =  ""; // Your code here 
+
+// Your code here
+// Set the corresponding <p> elements in the GetSessionData.html page with the values from above
+
+
---
+++ Lab_7/js/setSessionData.js
@@ -0,0 +1,17 @@
+const main = document.getElementById("main");
+
+function getClasses(){
+    
+    let classId =  ""; // Your code here
+
+    if(typeof classId !== "undefined" & classId !== ""){
+        
+        let classURL =  ""; // Your code here
+
+        // YOUR CODE HERE
+        
+    }
+    else{
+        main.innerHTML = "No value provided";
+    }
+}
