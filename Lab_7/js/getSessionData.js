e71c5c0bce4d4a814a5a966cd744ded510a2e026
ytang18 <dannytang913@gmail.com>
2020-11-06 19:20:59 -0800

    update

--- Lab_7/js/getSessionData.js
+++ Lab_7/js/getSessionData.js
@@ -1,9 +1,9 @@
 // Get saved data from sessionStorage
-let title = ""; // Your code here
-let lat_max = "";
-let lat_min = "";
-let lon_max = "";
-let lon_min = "";
+let title = "title"; // Your code here
+let lat_max = "lat_max";
+let lat_min = "lat_min";
+let lon_max = "lon_max";
+let lon_min = "lon_min";
 
 // Your code here
 // Set the corresponding <p> elements in the GetSessionData.html page with the values from above
